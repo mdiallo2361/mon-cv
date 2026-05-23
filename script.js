14f1016d5b69525eb495ee93d7ebdc5a1404fc7f
Mamadou Diallo <mdiallo2361@gmail.com>
2026-05-23 01:52:51 +0100

    premier commit - CV Mouhamadou Diallo

---
+++ script.js
@@ -0,0 +1,37 @@
+// Génération automatique des QR codes
+
+new QRCode(document.getElementById("qr-bac"), {
+  text: "https://drive.google.com/file/d/1Vq1mHMEJ6I6ZeQcN-i57WsW75sWzksqw/view?usp=sharing",
+  width: 80,
+  height: 80,
+  colorDark: "#1a1a2e",
+  colorLight: "#ffffff",
+  correctLevel: QRCode.CorrectLevel.H
+});
+
+new QRCode(document.getElementById("qr-deust"), {
+  text: "https://drive.google.com/file/d/1C8YoNRpTTgcEUQKH3e5P2EGo1c6x6tHt/view?usp=sharing",
+  width: 80,
+  height: 80,
+  colorDark: "#1a1a2e",
+  colorLight: "#ffffff",
+  correctLevel: QRCode.CorrectLevel.H
+});
+
+new QRCode(document.getElementById("qr-projet"), {
+  text: "https://drive.google.com/file/d/1-Xkb8uGVDVhuS5AEO5uWEYMvue1afUJO/view?usp=sharing",
+  width: 80,
+  height: 80,
+  colorDark: "#1a1a2e",
+  colorLight: "#ffffff",
+  correctLevel: QRCode.CorrectLevel.H
+});
+
+new QRCode(document.getElementById("qr-s5"), {
+  text: "TON_LIEN_ICI",
+  width: 80,
+  height: 80,
+  colorDark: "#1a1a2e",
+  colorLight: "#ffffff",
+  correctLevel: QRCode.CorrectLevel.H
+});
